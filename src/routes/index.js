1ecab599d14c881489933e8ccce3f99a9fd3ea08
LuisAndresParraAmaya <65096568+LuisAndresParraAmaya@users.noreply.github.com>
2021-09-16 00:30:41 -0300

    Update Backend

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,17 +11,23 @@ router.post('/CreateAccount', async (req, res) => {
 router.post('/Login', async (req, res) => {
     const { user_email, user_password } = req.body.data
     const response = await pool.query(`SELECT * FROM user WHERE user_email="${user_email}" AND user_password="${user_password}" AND user_status="enabled"`)
-    if (response.length > 0) res.json(response[0])
+    if (response.length > 0) {
+        res.json(response[0].user_rut)
+    }
     else res.json({'Response': 'Login Failed'})
 })
 
 router.post('/ModifyProfile', async (req, res) => {
     //RECIBIR DESDE SESSION CLIENTE. -> USER RUT CURRENT
-    const { user_new_rut, user_name, user_last_name, user_email, user_phone } = req.body.data
+    const { user_new_rut, user_rut, user_name, user_last_name, user_email, user_phone } = req.body.data
     const response = await pool.query(`UPDATE user
-    SET user_rut = ${user_new_rut}, user_name = ${user_name}, user_last_name = ${user_last_name}, user_email = ${user_email}, user_phone = ${user_phone}
-    WHERE user_rut = 195915741"`)
-    if (response.length > 0) res.json({'Response': 'Login Success'})
-    else res.json({'Response': 'Login Failed'})
+    SET user_rut = ${user_new_rut}, user_name = "${user_name}", user_last_name = "${user_last_name}", user_email = "${user_email}", user_phone = ${user_phone}
+    WHERE user_rut = ${user_rut}`)
+    console.log(response)
+    if(response.affectedRows>0){
+        res.json({'user_new_rut': user_new_rut})
+    } else {
+        res.json({'Response': 'Operation Failed'})
+    }
 })
 module.exports = router
