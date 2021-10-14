ef95ebb16b0baf415cf0b46cc8a645f315fe3a50
LuisAndresParraAmaya <LuisAndresParraAmaya@gmail.com>
2021-10-13 23:52:03 -0300

    mas cambios

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,8 +11,8 @@ router.post('/CreateAccount', async (req, res) => {
         user_password, user_status
     } = req.body.data
     // Proceso de encriptación
-    bcryptjs.genSalt(10, async function(err) {
-        bcryptjs.hash(user_password, 7, async function(err, hash) {
+    bcryptjs.genSalt(10, async function (err) {
+        bcryptjs.hash(user_password, 7, async function (err, hash) {
             try {
                 await pool.query(`INSERT INTO user (user_rut, user_type_id, user_name, user_last_name, user_email, user_phone, user_password, user_status) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
                     [`${user_rut}`, `${user_type_id}`, `${user_name}`, `${user_last_name}`, `${user_email}`, `${user_phone}`, `${hash}`, `${user_status}`])
@@ -42,35 +42,35 @@ router.post('/Login', async (req, respuesta) => {
     // Traer contraseña encriptada del usuario
     console.log(user_email, user_password)
     const response = await pool.query('SELECT * FROM `user` WHERE `user_email` = ?', [`${user_email}`]);
-    if(response.length > 0){
-        bcryptjs.compare(user_password, response[0].user_password, function(err, res) {
-            if(res){
-                if(response[0].user_status == 'disabled'){
-                    respuesta.json({ 'Response': 'Account disabled'})
+    if (response.length > 0) {
+        bcryptjs.compare(user_password, response[0].user_password, function (err, res) {
+            if (res) {
+                if (response[0].user_status == 'disabled') {
+                    respuesta.json({ 'Response': 'Account disabled' })
                 }
-                if(response[0].user_status == 'deleted'){
-                    respuesta.json({ 'Response': 'Account deleted'})
+                if (response[0].user_status == 'deleted') {
+                    respuesta.json({ 'Response': 'Account deleted' })
                 }
-                if(response[0].user_status == 'enabled'){
-                    respuesta.json({ 
-                        'Response': 'Login Success', 
-                        'user_rut': response[0].user_rut, 
-                        'user_name': response[0].user_name, 
-                        'user_last_name': response[0].user_last_name, 
-                        'user_phone': response[0].user_phone, 
-                        'user_email': response[0].user_email, 
-                        'user_password': response[0].user_password, 
+                if (response[0].user_status == 'enabled') {
+                    respuesta.json({
+                        'Response': 'Login Success',
+                        'user_rut': response[0].user_rut,
+                        'user_name': response[0].user_name,
+                        'user_last_name': response[0].user_last_name,
+                        'user_phone': response[0].user_phone,
+                        'user_email': response[0].user_email,
+                        'user_password': response[0].user_password,
                         'user_type_id': response[0].user_type_id
                     })
                 }
-            }else{
+            } else {
                 respuesta.json({ 'Response': 'Login Failed' })
             }
         });
-    }else{
+    } else {
         respuesta.json({ 'Response': 'Login Failed' })
     }
-    
+
 })
 
 router.post('/ModifyProfile', async (req, respuesta) => {
@@ -78,8 +78,8 @@ router.post('/ModifyProfile', async (req, respuesta) => {
     const { user_new_rut, user_rut, user_name, user_last_name, user_email, user_phone, user_password } = req.body.data
     console.log(user_password)
     const response = await pool.query('SELECT * FROM `user` WHERE `user_rut` = ?', [`${user_rut}`]);
-    bcryptjs.compare(user_password, response[0].user_password, async function(err, res) {
-        if(res){
+    bcryptjs.compare(user_password, response[0].user_password, async function (err, res) {
+        if (res) {
             const response = await pool.query(`UPDATE user
             SET user_rut = ?, user_name = ?, user_last_name = ?, user_email = ?, user_phone = ?
             WHERE user_rut = ?`, [`${user_new_rut}`, `${user_name}`, `${user_last_name}`, `${user_email}`, `${user_phone}`, `${user_rut}`])
@@ -88,7 +88,7 @@ router.post('/ModifyProfile', async (req, respuesta) => {
             } else {
                 respuesta.json({ 'Response': 'Operation Failed' })
             }
-        }else{
+        } else {
             respuesta.json({ 'Response': 'Actual Password Failed' })
         }
     });
@@ -98,10 +98,10 @@ router.post('/VerifyPasswordCorrect', async (req, respuesta) => {
     //RECIBIR DESDE SESSION CLIENTE. -> USER RUT CURRENT
     const { user_password, user_email } = req.body.data
     const response = await pool.query('SELECT * FROM `user` WHERE `user_email` = ?', [`${user_email}`]);
-    bcryptjs.compare(user_password, response[0].user_password, async function(err, res) {
-        if(res){
+    bcryptjs.compare(user_password, response[0].user_password, async function (err, res) {
+        if (res) {
             respuesta.json({ 'Response': 'Actual Password Success' })
-        }else{
+        } else {
             respuesta.json({ 'Response': 'Actual Password Failed' })
         }
     });
@@ -150,11 +150,11 @@ router.post('/ModifyPassword', async (req, respuesta) => {
     const { user_rut, user_password, user_new_password } = req.body.data
     // COMPARAR CONTRASEÑAS
     const response = await pool.query('SELECT * FROM `user` WHERE `user_rut` = ?', [`${user_rut}`]);
-    if(user_password !== undefined){
-        bcryptjs.compare(user_password, response[0].user_password, function(err, res) {
-            if(res){
-                bcryptjs.genSalt(10, async function(err) {
-                    bcryptjs.hash(user_new_password, 7, async function(err, hash) {
+    if (user_password !== undefined) {
+        bcryptjs.compare(user_password, response[0].user_password, function (err, res) {
+            if (res) {
+                bcryptjs.genSalt(10, async function (err) {
+                    bcryptjs.hash(user_new_password, 7, async function (err, hash) {
                         await pool.query(`UPDATE user SET user_password = ? WHERE user_rut = ?`, [`${hash}`, `${user_rut}`])
                         respuesta.json({ 'Response': 'Operation Success' })
                     })
@@ -163,9 +163,9 @@ router.post('/ModifyPassword', async (req, respuesta) => {
                 respuesta.json({ 'Response': 'Actual Password Failed' })
             }
         })
-    }else{
-        bcryptjs.genSalt(10, async function(err) {
-            bcryptjs.hash(user_new_password, 7, async function(err, hash) {
+    } else {
+        bcryptjs.genSalt(10, async function (err) {
+            bcryptjs.hash(user_new_password, 7, async function (err, hash) {
                 await pool.query(`UPDATE user SET user_password = ? WHERE user_rut = ?`, [`${hash}`, `${user_rut}`])
                 respuesta.json({ 'Response': 'Operation Success' })
             })
@@ -179,10 +179,10 @@ router.post('/DisableAccount', async (req, respuesta) => {
     const { user_rut, user_password } = req.body.data
     // COMPARAR CONTRASEÑAS
     const response = await pool.query('SELECT * FROM `user` WHERE `user_rut` = ?', [`${user_rut}`]);
-    bcryptjs.compare(user_password, response[0].user_password, async function(err, res) {
-        if(res){
-                await pool.query(`UPDATE user SET user_status = ? WHERE user_rut = ?`, [`disabled`, `${user_rut}`])
-                respuesta.json({ 'Response': 'Operation Success' })
+    bcryptjs.compare(user_password, response[0].user_password, async function (err, res) {
+        if (res) {
+            await pool.query(`UPDATE user SET user_status = ? WHERE user_rut = ?`, [`disabled`, `${user_rut}`])
+            respuesta.json({ 'Response': 'Operation Success' })
         } else {
             respuesta.json({ 'Response': 'Actual Password Failed' })
         }
@@ -262,34 +262,35 @@ router.post('/AddWorkshopOffice', async (req, res) => {
     const { workshop_id, commune_id, workshop_suscription_id, workshop_office_address, workshop_office_phone, workshop_office_attention } = req.body.data
     let values = ''
     try {
-    const statement = `INSERT INTO workshop_office (workshop_id, commune_id, workshop_suscription_id, workshop_office_address, workshop_office_phone)
+        const statement = `INSERT INTO workshop_office (workshop_id, commune_id, workshop_suscription_id, workshop_office_address, workshop_office_phone)
     VALUES (?, ?, ?, ?, ?)`
-    const response = await pool.query(statement, [`${workshop_id}`, `${commune_id}`, `${workshop_suscription_id}`, `${workshop_office_address}`, `${workshop_office_phone}`])
-    if (response.affectedRows > 0) {
-        for (let i = 0; i < workshop_office_attention.length; i++) {
-            let day = workshop_office_attention[i].workshop_office_attention_day
-            let aperture = workshop_office_attention[i].workshop_office_attention_aperture_time
-            let departure = workshop_office_attention[i].workshop_office_attention_departure_time
-            // Aca va el identificador de la sucursal
-            let myrow = `(${response.insertId}, "${day}", "${aperture}", "${departure}")`
-            //SI ESTAMOS EN LA ULTIMA ITERACION NO SE LE AGREGA LA COMA AL FINAL DEL STRING
-            if (i == (workshop_office_attention.length - 1)) {
-                values = values.concat(myrow)
-            } else {
-                values = values.concat(myrow + ',')
+        const response = await pool.query(statement, [`${workshop_id}`, `${commune_id}`, `${workshop_suscription_id}`, `${workshop_office_address}`, `${workshop_office_phone}`])
+        if (response.affectedRows > 0) {
+            for (let i = 0; i < workshop_office_attention.length; i++) {
+                let day = workshop_office_attention[i].workshop_office_attention_day
+                let aperture = workshop_office_attention[i].workshop_office_attention_aperture_time
+                let departure = workshop_office_attention[i].workshop_office_attention_departure_time
+                // Aca va el identificador de la sucursal
+                let myrow = `(${response.insertId}, "${day}", "${aperture}", "${departure}")`
+                //SI ESTAMOS EN LA ULTIMA ITERACION NO SE LE AGREGA LA COMA AL FINAL DEL STRING
+                if (i == (workshop_office_attention.length - 1)) {
+                    values = values.concat(myrow)
+                } else {
+                    values = values.concat(myrow + ',')
+                }
             }
         }
-    } } catch (exception) {
+    } catch (exception) {
         if (exception.sqlMessage.includes(workshop_office_address)) {
-            res.json({ 'Response': 'Address already in use'})
+            res.json({ 'Response': 'Address already in use' })
             return
         }
     }
-        const statement2 = `INSERT INTO workshop_office_attention (workshop_office_id, workshop_office_attention_day, workshop_office_attention_aperture_time, workshop_office_attention_departure_time) VALUES ${values}`
-        const response2 = await pool.query(statement2)
-        if (response2.affectedRows > 0) {
-            res.json({ 'Response': 'Office Attention Success' })
-        }
+    const statement2 = `INSERT INTO workshop_office_attention (workshop_office_id, workshop_office_attention_day, workshop_office_attention_aperture_time, workshop_office_attention_departure_time) VALUES ${values}`
+    const response2 = await pool.query(statement2)
+    if (response2.affectedRows > 0) {
+        res.json({ 'Response': 'Office Attention Success' })
+    }
 })
 
 
@@ -373,4 +374,42 @@ router.post('/MyWorkshopOfficeAttention', async (req, res) => {
     }
     else res.json({ 'Response': 'Attention Not Found' })
 })
+
+//UPDATE user set user_type_id = 4 WHERE user_rut = 195915741
+router.post('/AddWorkshopOfficeEmployee', async (req, res) => {
+    const { workshop_office_id, user_rut, workshop_office_employee_specialization, workshop_office_employee_experience } = req.body.data
+    try {
+        const statement = `INSERT INTO workshop_office_employee (workshop_office_id, user_rut, workshop_office_employee_specialization, workshop_office_employee_experience) VALUES (?, ?, ?, ?)`
+        const response = await pool.query(statement, [`${workshop_office_id}`, `${user_rut}`, `${workshop_office_employee_specialization}`, `${workshop_office_employee_experience}`])
+        if (response.affectedRows > 0) {
+            const statement2 = `UPDATE user set user_type_id = ? WHERE user_rut = ?`
+            await pool.query(statement2, [`4`, `${user_rut}`])
+            res.json({ 'Response': 'Operation Success' })
+        }
+    } catch (exception) {
+        const errorSQL = exception.sqlMessage
+        if (errorSQL.includes('foreign key')) {
+            res.json({ 'Response': 'Rut not exist' })
+            return
+        }
+    }
+})
+
+router.post('/AddWorkshopOfficeService', async (req, res) => {
+    const { workshop_office_id, offer_id, workshop_office_service_name, workshop_office_service_price, workshop_office_service_estimated_time, workshop_office_service_description } = req.body.data
+    try {
+        const statement = `INSERT INTO workshop_office_service (workshop_office_id, 
+            offer_id, workshop_office_service_name, workshop_office_service_price,
+            workshop_office_service_estimated_time, workshop_office_service_description
+           ) values (?, ?, ?, ?, ?, ?)`
+        const response = await pool.query(statement, [`${workshop_office_id}`, `${offer_id}`, `${workshop_office_service_name}`, `${workshop_office_service_price}`, `${workshop_office_service_estimated_time}`, `${workshop_office_service_description}`])
+        if (response.affectedRows > 0) {
+            res.json({ 'Response': 'Operation Success' })
+        }
+    } catch (exception) {
+        res.json({ 'Response': exception.sqlMessage })
+    }
+})
+
+
 module.exports = router
